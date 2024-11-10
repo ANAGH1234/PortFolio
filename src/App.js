ba4a70773e29ad1184aec47abe728751e58ff4fd
Demon12 <78343939+ANAGH1234@users.noreply.github.com>
2024-11-10 22:07:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import Navbar from "./scenes/Navbar";
 import Landing from "./scenes/Landing";
 import DotGroup from "./scenes/DotGroup";
-import MyPhotos from "./scenes/MyPhotos";
+import MySkills from "./scenes/MySkills";
 import LineGradient from "./components/LineGradient";
 import Projects from "./scenes/Projects";
 import Contact from "./scenes/Contact";
 import Footer from "./scenes/Footer";
 import useMediaQuery from "./hooks/useMediaQuery";
 import { useEffect, useState } from "react";
-import Testimonials from "./scenes/Testimonials";
 import { motion } from "framer-motion";
 
 function App() {
@@ -55,13 +54,21 @@ function App() {
         <motion.div
           margin="0 0 -200px 0"
           amount="all"
-          onViewportEnter={() => setSelectedPage("Photos")}
+          onViewportEnter={() => setSelectedPage("skills")}
         >
-          <MyPhotos />
+          <MySkills />
         </motion.div>
       </div>
-     
-      
+      <LineGradient />
+      <div className="w-5/6 mx-auto">
+        <motion.div
+          margin="0 0 -200px 0"
+          amount="all"
+          onViewportEnter={() => setSelectedPage("projects")}
+        >
+          <Projects />
+        </motion.div>
+      </div>
       <LineGradient />
       
       <LineGradient />
