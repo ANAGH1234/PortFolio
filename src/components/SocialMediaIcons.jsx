eee2b5b2350052bfdf193fb22ed8343dd42d7974
Demon12 <78343939+ANAGH1234@users.noreply.github.com>
2024-11-10 21:57:30 +0530

    Update SocialMediaIcons.jsx

--- src/components/SocialMediaIcons.jsx
+++ src/components/SocialMediaIcons.jsx
@@ -3,7 +3,7 @@ const SocialMediaIcons = () => {
     <div className="flex justify-center md:justify-start my-10 gap-7">
       <a
         className="hover:opacity-50 transition duration-500"
-        href="https://www.linkedin.com/in/dinesh-sethi-930342132/"
+        href="https://www.linkedin.com/in/anagh-sethi-44104521a/"
         target="_blank"
         rel="noreferrer"
       >
@@ -11,7 +11,7 @@ const SocialMediaIcons = () => {
       </a>
       <a
         className="hover:opacity-50 transition duration-500"
-        href="https://www.instagram.com/dinesh.sethi.545/"
+        href="https://www.instagram.com/anagh_sethi/"
         target="_blank"
         rel="noreferrer"
       >
